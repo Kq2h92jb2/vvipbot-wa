c8888da12a740b958355a0cf98d13c6d4f3e5ce5
Kq2h92jb2 <166942398+Kq2h92jb2@users.noreply.github.com>
2024-04-14 17:35:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Hana Khoi\n' // Nama kamu
+            + 'ORG:Hantwo Bot;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6283146845711:+62 831-4684-5711\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
